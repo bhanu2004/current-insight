6d4e6dffa1729d0624c1cf7429bc216f7282fff0
bhanu2004 <bhanuprksh05@gmail.com>
2023-11-27 00:36:06 +0530

    fds

--- app/providers/AuthProvider.jsx
+++ app/providers/AuthProvider.jsx
@@ -3,8 +3,11 @@
 import {SessionProvider} from "next-auth/react";
 const AuthProvider = ({children})=>{
     return (
-        <SessionProvider>{children}</SessionProvider>
+        <SessionProvider basePath="/api/auth">{children}</SessionProvider>
     )
 }
 
 export default AuthProvider;
+
+// git-main-bhanu2004.vercel.app
+// current-insight-emh2qsslh-bhanu2004.vercel.app
